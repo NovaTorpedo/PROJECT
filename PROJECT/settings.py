1ad9c22526d1d8ddd192a5c562e36e25299de347
Joseph Odipo <103773790+NovaTorpedo@users.noreply.github.com>
2024-04-22 13:58:30 +0300

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'smvs',
          'USER': 'root',
-         'PASSWORD': 'Jay21541',
+         'PASSWORD': '',
          'HOST': 'localhost',  # or the hostname where your MySQL server is running
          'PORT': '3306',      # or the port on which your MySQL server is listening
      }
